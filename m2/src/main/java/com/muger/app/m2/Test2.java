2481076590a90131019092d8eb35d1c091b90753
朱德鸿 <zhudehong@tuoluoshijie.com>
2021-06-28 18:39:38 +0800

    init

---
+++ m2/src/main/java/com/muger/app/m2/Test2.java
@@ -0,0 +1,12 @@
+package com.muger.app.m2;
+
+/**
+ * @author zhudehong
+ */
+
+public class Test2 {
+
+    public static void main(String[] args) {
+        System.out.println("hello demo2");
+    }
+}
